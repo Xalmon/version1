3e49e7e41d8d7c0aa4a89cce9daacd2bffb88953
Xalmon <xalmonoyefule@gmail.com>
2024-08-15 13:52:53 +0100

    test commit

---
+++ src/main/java/com.example.version1/version1Application.java
@@ -0,0 +1,12 @@
+package com.example.version1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class version1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(version1Application.class, args);
+  }
+}
